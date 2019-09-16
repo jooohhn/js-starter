ed33eff9e94e94c49457604ffa76bcfea1d100be
John Tran <jooohhn@users.noreply.github.com>
2019-09-15 23:18:59 -0700

    Infrastructure upgrade (#8)
    * Removed package-lock.json
    
    * Rebuilt lock file
    
    * Updated webpack
    
    * Changed ports to 8000
    
    * Removed babel-polyfill
    
    * Removed old babel deps
    
    * Updated flow and babel
    
    * Updated linters and jest
    
    * Updated prod build deps
    
    * Deleted hello page
    
    * Rearranged file names and directories
    
    * Updated travis

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,5 @@
 // @flow
 
-import 'babel-polyfill';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
--- src/shared/config.js
+++ src/shared/config.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const WEB_PORT = process.env.PORT || 8080;
+export const WEB_PORT = process.env.PORT || 8000;
 export const STATIC_PATH = '/static';
 export const APP_NAME = 'Hello App';
 
